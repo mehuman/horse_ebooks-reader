f8aef88e3a16fa07464fcc7ab47088b73a8b0d49
Mark Huson <mehuman@gmail.com>
2012-01-21 20:42:39 -0500

    Initial Commit

---
+++ horse_ebooks-reader.py
@@ -0,0 +1,12 @@
+import twitter
+import subprocess
+import time
+api = twitter.Api()
+
+statuses = api.GetUserTimeline('horse_ebooks')
+#subprocess.call(['/usr/bin/say', [s.text for s in statuses]])
+for s in statuses:
+	subprocess.call(['/usr/bin/say','A horse ebooks tweet'])
+	time.sleep(1)
+	subprocess.call(['/usr/bin/say',s.text])
+	time.sleep(2)
